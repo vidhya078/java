7ce9b717df1e247839c50ddfd53da3c5dc9a4150
vidhya078 <vidhyababu078@gmail.com>
2025-03-12 14:49:15 +0530

    bs

---
+++ concat.java
@@ -0,0 +1,9 @@
+public class concat
+{
+    public static void main(String args[])
+    {
+    String firstname="vidhya";
+    String lastname="Babu";
+    System.out.println(firstname.concat(lastname));
+    }
+}
