5799aa05581975ceffb278c071bb948f7b2567a0
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 15:16:10 +0530

    zzz

---
+++ nest.java
@@ -0,0 +1,22 @@
+public class nest 
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=20;
+        if (a==b)
+        {
+            System.out.println("a and b are equal");
+        }
+        else if (a>=b)
+        {
+            System.out.println("a is greater than b");
+        }
+        else
+        {
+            System.out.println("a is less than b");
+        }
+
+    }
+    
+}
