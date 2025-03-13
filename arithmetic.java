adc10e8ca1d20528c85e90d47112397399e396d0
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 11:11:21 +0530

    addsub

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+public class arithmetic {
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        System.out.println("enter the value c:"+c);
+        System.out.println("enter the value d:"+d);
+        System.out.println("enter the value e:"+e);
+    }
+}
