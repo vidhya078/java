9993b8647a5e413973c677f61fba38fd619acfcc
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 11:13:50 +0530

    detials

---
+++ student.java
@@ -0,0 +1,13 @@
+public class student {
+    public static void main(String args[])
+    {
+    String name="Vidhya";
+    String place= "Kulasekharam";
+    int mark=98;
+    char grade='A';
+    System.out.println("Enter the name:"+name); 
+    System.out.println("Enter the Place:"+place); 
+    System.out.println("Enter the Mark:"+mark);
+    System.out.println("Enter the Grade:"+grade); 
+    } 
+}
