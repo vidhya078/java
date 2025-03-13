3d6ba325ff4f38e173f5afb72cfc6ba16eedce4d
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 15:36:02 +0530

    hai

---
+++ one.java
@@ -0,0 +1,13 @@
+public class one 
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        if(a==10)
+        System.out.println("a is equal to 10");
+        if(a>10)
+        System.out.println("a is greater than 10");
+        if(a<10)
+        System.out.println("a is smaller than 10");
+    }
+}
