80890bad38aacdc20a23d824fd51c8ae48c43c54
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 13:44:22 +0530

    xyz

---
+++ swap.java
@@ -0,0 +1,12 @@
+public class swap 
+{
+    public static void main(String[]args)
+    {
+    int a=5;
+    int b=6;
+    a=(5+6)-5;
+    b=(5+6)-6;
+    System.out.println("a="+a);
+    System.out.println("b="+b);
+    }
+}
