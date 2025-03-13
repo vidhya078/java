3ccce84c1416639c963062b39ea3f23f1076e3a9
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 14:39:15 +0530

    num

---
+++ age.java
@@ -0,0 +1,11 @@
+public class age
+{
+    public static void main(String[]args)
+    {
+    int age=19;
+    if(age>=18)
+    System.out.println("Eligible");
+    else
+    System.out.println("noteligible");
+}
+}
