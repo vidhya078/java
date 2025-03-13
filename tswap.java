c19d25c7109fb961efebd203550543cd39e0741c
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 13:55:31 +0530

    ooo

---
+++ tswap.java
@@ -0,0 +1,15 @@
+public class tswap
+ {
+    public static void main(String []args)
+    {
+        int a=5;
+        int temp;
+        int b=6;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+    
+}
