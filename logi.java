fd261b6ce39685bafe45d15aa3a3fe7b1a007302
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 12:11:10 +0530

    xx

---
+++ logi.java
@@ -0,0 +1,18 @@
+public class logi 
+{
+    public static void main (String[]args)
+    {
+        int a=88;
+        int b=88;
+        int rollnoA=01;
+        int rollno=02;
+        if(a<b&&rollnoA==rollno)
+        {
+            System.out.println("True");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+    
+}
