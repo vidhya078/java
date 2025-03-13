33a4a3218fe88dfe576ed93668d0b2578d0fa934
vidhya078 <vidhyababu078@gamil.com>
2025-03-13 12:12:34 +0530

    bb

---
+++ comp.java
@@ -0,0 +1,16 @@
+public class comp
+ {
+    public static void main(String []args)
+    {
+   String a ="Today is Monday";
+   String b="Today is Tuesday";
+   if(a==b)
+   { 
+    System.out.println("True");
+   }
+    else
+    {
+    System.out.println("False");
+}
+ }
+}
